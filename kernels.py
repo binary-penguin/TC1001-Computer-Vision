336b4545feaf28e3b2abc556fa1987d42f1299e7
Juan Pablo Ortiz <a01366969@itesm.mx>
2022-03-25 08:46:48 -0600

    Add kernels to main

--- kernels.py
+++ kernels.py
@@ -11,39 +11,8 @@ def mexican_hat(size):
       inner_calc = (-1 * inner_calc) / 2
       A[x][y] = numpy.exp(inner_calc)
 
+  # d. Return matrix
   return A
-
-
-def vh():
-
-    k0 = numpy.array([[0,-1,0],[-1,0,1],[0,1,0]])
-
-    '''
-    1 -1 1
-    -1 4 -1
-    1 -1 1
-    '''
-    return k0
-    
-def horizontal():
-    k1 = numpy.array([[0,0,0],[-1,0,1],[0,0,0]])
-
-    '''
-    0 0 0
-    -1 0 1
-    0 0 0
-    '''
-    return k1
-
-def vertical():
-    k2 = numpy.array([[0,-1,0],[0,0,0],[0,1,0]])
-
-    '''
-    0 -1 0
-    0  0 0
-    0  1 0
-    '''
-    return k2
 
 def gaussian(size):
   # a. Define sigma value 
--- main.py
+++ main.py
@@ -11,15 +11,11 @@ if __name__ == "__main__":
     I = numpy.asarray(I);
     I = I / 255.0;
 
-    k0 = vh()
-    k1 = horizontal()
-    k2 = vertical()
-    k3 = mexican_hat(3)
+    k0 = mexican_hat(9)
+    k1 = gaussian(9)
 
     J0 = ndimage.convolve(I, k0, mode='constant', cval=0.0)
     J1 = ndimage.convolve(I, k1, mode='constant', cval=0.0)
-    J2 = ndimage.convolve(I, k2, mode='constant', cval=0.0)
-    J3 = ndimage.convolve(I, k2, mode='constant', cval=0.0)
 
     plt.figure(figsize = (15,15))
 
@@ -29,19 +25,11 @@ if __name__ == "__main__":
 
     plt.subplot(3,3,2)
     plt.imshow(J0)
-    plt.xlabel('VH direction')
+    plt.xlabel('Mexican Hat')
 
     plt.subplot(3,3,3)
     plt.imshow(J1)
-    plt.xlabel('Horizontal direction')
-
-    plt.subplot(3,3,4)
-    plt.imshow(J2)
-    plt.xlabel('Vertical direction')
-
-    plt.subplot(3,3,5)
-    plt.imshow(J3)
-    plt.xlabel('Mexican Hat')
+    plt.xlabel('Gaussian blur')
 
 
     plt.grid(False)
